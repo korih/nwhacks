857a8b1e6150af1d811601fb7ea12dca02dac7a4
korih <96402323+korih@users.noreply.github.com>
2025-01-19 11:06:56 -0800

    clear chat feature

--- frontend/src/components/OldChats.tsx
+++ frontend/src/components/OldChats.tsx
@@ -1,10 +1,23 @@
-export default function OldChats() {
+export default function OldChats({ clearCurrentChat }: { clearCurrentChat: () => void }) {
+
+    const handleCreateNewChat = () => {
+        console.log("hello")
+        console.log(typeof clearCurrentChat)
+        clearCurrentChat()
+    }
+
     return (
         <div className="w-[25%] bg-[#1B1B1D] flex flex-col justify-start items-center">
             <h2 className="mt-2 font-bold text-4xl text-[#f2f0ef]">Chats</h2>
             <div className="h-full w-full">
+                <h2 
+                    className="mt-4 mx-5 text-[#f2f0ef] justify-start items-start border-2 border-[#949392] p-1 rounded-lg cursor-pointer"
+                    onClick={handleCreateNewChat}
+                >
+                   Create a new Chat 💻 
+                </h2>
                 <h2 className="mt-4 mx-5 text-[#f2f0ef] justify-start items-start border-2 border-[#949392] p-1 rounded-lg cursor-pointer">
-                    TestChat
+                    Current Chat
                 </h2>
             </div>
         </div>
