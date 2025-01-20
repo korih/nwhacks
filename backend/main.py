ab8919863f4fe4ccdffbbeccf5fb769314d9576a
Arman D <adrismir@gmail.com>
2025-01-19 22:15:23 -0800

    use port 8000

--- backend/main.py
+++ backend/main.py
@@ -30,4 +30,4 @@ app.include_router(chat)
 
 if __name__ == "__main__":
 	init_db()
-	uvicorn.run("main:app", host="0.0.0.0", port=8100, reload=True)
+	uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
